0ef1917d663c9179bec1fb93d95ff65badaa89b8
Abdulloh <maxmudovabdullo97@gmail.com>
2020-07-21 12:40:31 +0500

    Initial commit

---
+++ app/src/main/java/myway/telegram/MainActivity.kt
@@ -0,0 +1,53 @@
+package myway.telegram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import myway.telegram.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mDrawer: Drawer
+    private lateinit var mHeader: AccountHeader
+    private lateinit var mToolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        // setContentView(R.layout.activity_main)
+        setContentView(mBinding.root)
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunc()
+    }
+
+    private fun initFunc() {
+        setSupportActionBar(mToolbar)
+        createHeader()
+    }
+
+    private fun createHeader() {
+        mHeader = AccountHeaderBuilder()
+            .withActivity(this)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                ProfileDrawerItem().withName("Makhmudov")
+                    .withEmail("+998933769197")
+            ).build()
+    }
+
+    private fun initFields() {
+        mToolbar = mBinding.mainToolbar
+
+    }
+}
