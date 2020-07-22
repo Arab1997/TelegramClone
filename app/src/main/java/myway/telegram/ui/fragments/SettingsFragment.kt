694d4adef41eabb1aa85939d239286713928ad6e
Abdulloh <maxmudovabdullo97@gmail.com>
2020-07-22 12:42:14 +0500

    lesson 7

--- app/src/main/java/myway/telegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/myway/telegram/ui/fragments/SettingsFragment.kt
@@ -1,5 +1,7 @@
 package myway.telegram.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import myway.telegram.R
 
 
@@ -7,6 +9,12 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_actions_menu, menu)
+
     }
 
 
